2cdeb7553715c55bb8a96e8ae686c9ddd7e74270
saman shaiza <samanshaiza@yahoo.com>
2026-01-01 19:18:09 -0600

    moved objective detail before histroy section

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { AboutHero } from './about-hero';
-import { HistorySection } from './history-section';
-import { FoundersSection } from './founders-section';
-import { ObjectivesDetail } from './objectives-detail';
+import React from "react";
+import { AboutHero } from "./about-hero";
+import { HistorySection } from "./history-section";
+import { FoundersSection } from "./founders-section";
+import { ObjectivesDetail } from "./objectives-detail";
 
 const About: React.FC = () => {
   return (
     <div className="about">
       <AboutHero />
+      <ObjectivesDetail />
       <HistorySection />
       <FoundersSection />
-      <ObjectivesDetail />
     </div>
   );
 };
